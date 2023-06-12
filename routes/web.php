af96bdb79a53215d533763d7c24c2beece8243f8
Fauzun Naja <70993035+Funazu@users.noreply.github.com>
2023-06-12 10:36:44 +0700

    restructure route and implementasi middleware

--- routes/web.php
+++ routes/web.php
@@ -30,31 +30,36 @@ Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth
 Route::get('/auth/logout', [AuthController::class, 'logout'])->middleware('auth');
 
 // Home
-Route::get('/', [TamuController::class, 'index']);
+Route::get('/', [TamuController::class, 'index'])->name('user');
 Route::post('/tamu/create', [TamuController::class, 'bukuTamuPost']);
 Route::get('/tamu/create/success/{BukuTamu:uniqid}', [TamuController::class, 'qrcode']);
 Route::get('/tamu/{BukuTamu:uniqid}', [TamuController::class, 'tamu']);
 
+// Middleware Admin
+Route::middleware('admin')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [DashboardController::class, 'profile'])->name('profile');
+    Route::put('/auth/account/changepassword/{user:id}', [AuthController::class, 'changepassword']);
+    
+    // Buku Tamu
+    Route::get('/dashboard/bukutamu', [BukuTamuController::class, 'index'])->name('bukuTamu');
+    Route::post('/dashboard/bukutamu/create', [BukuTamuController::class, 'bukuTamuPost']);
+    Route::put('/dashboard/bukutamu/edit/{BukuTamu:id}', [BukuTamuController::class, 'bukuTamuEdit']);
+    Route::post('/dashboard/bukutamu/delete/{BukuTamu:id}', [BukuTamuController::class, 'bukuTamuDelete']);
+    
+    // Tamu Undangan
+    Route::get('/dashboard/tamu-undangan', [TamuUndanganController::class, 'index'])->name('tamuUndangan');
+    Route::post('/dashboard/tamu-undangan/create', [TamuUndanganController::class, 'tamuUndanganPost']);
+    Route::put('/dashboard/tamu-undangan/edit/{TamuUndangan:id}', [TamuUndanganController::class, 'tamuUndanganEdit']);
+    Route::post('/dashboard/tamu-undangan/delete/{TamuUndangan:id}', [TamuUndanganController::class, 'tamuUndanganDelete']);
+    
+    Route::get('/tamu-undangan/{TamuUndangan:uniqid}', [TamuUndanganController::class, 'scanQrCode']);
+    
+    Route::get('/dashboard/tamu-undangan/scan', [TamuUndanganController::class, 'scanBarcode'])->name('scanBarcode');
+    Route::put('/dashboard/tamu-undangan/scan', [TamuUndanganController::class, 'scanBarcodePost']);
+});
 
-Route::put('/auth/account/changepassword/{user:id}', [AuthController::class, 'changepassword'])->middleware('auth');
-
-// Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::get('/profile', [DashboardController::class, 'profile'])->name('profile')->middleware('auth');
-
-// Buku Tamu
-Route::get('/dashboard/bukutamu', [BukuTamuController::class, 'index'])->name('bukuTamu')->middleware('auth');
-Route::post('/dashboard/bukutamu/create', [BukuTamuController::class, 'bukuTamuPost'])->middleware('auth');
-Route::put('/dashboard/bukutamu/edit/{BukuTamu:id}', [BukuTamuController::class, 'bukuTamuEdit'])->middleware('auth');
-Route::post('/dashboard/bukutamu/delete/{BukuTamu:id}', [BukuTamuController::class, 'bukuTamuDelete'])->middleware('auth');
-
-// Tamu Undangan
-Route::get('/dashboard/tamu-undangan', [TamuUndanganController::class, 'index'])->name('tamuUndangan')->middleware('auth');
-Route::post('/dashboard/tamu-undangan/create', [TamuUndanganController::class, 'tamuUndanganPost'])->middleware('auth');
-Route::put('/dashboard/tamu-undangan/edit/{TamuUndangan:id}', [TamuUndanganController::class, 'tamuUndanganEdit'])->middleware('auth');
-Route::post('/dashboard/tamu-undangan/delete/{TamuUndangan:id}', [TamuUndanganController::class, 'tamuUndanganDelete'])->middleware('auth');
-
-Route::get('/tamu-undangan/{TamuUndangan:uniqid}', [TamuUndanganController::class, 'scanQrCode']);
-
-Route::get('/dashboard/tamu-undangan/scan', [TamuUndanganController::class, 'scanBarcode'])->name('scanBarcode')->middleware('auth');
-Route::put('/dashboard/tamu-undangan/scan', [TamuUndanganController::class, 'scanBarcodePost'])->middleware('auth');
+Route::middleware('superadmin')->group(function () {
+    Route::get('/dashboard/makeuser', [DashboardController::class, 'makeUser'])->name('superadmin');
+});
